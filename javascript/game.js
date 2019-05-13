b7b16e09ac6d4d926698357a6ab9199ad41d92f0
unknown <fsadler9@gmail.com>
2019-05-12 20:06:09 -0600

    passes tests in console log- needs crystals score to upload to page

--- javascript/game.js
+++ javascript/game.js
@@ -1,128 +1,74 @@
-$(document).ready(function (){
-    //Global Variables: wins, losses,target score
-var wins = 0;
-var losses =0; 
-var counter= 0;
-
-//array of crystals (create variables) that you loop through- set max 1-12
-for (var i=0; i<5; i++) {
-
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-
-}
-
-//dynamically create crystals on the page 
-    //each crytal needs a score, an image(aka an image held as an string held in your images folder), an object with all of this info aka collection of key value pairs 
-
-//Random Number Function: 
-    //return a random number 
-    
-    function randomNum = 
-
-//Click Event Function: 
-    //each crystal should have same class 
-    // var score = yourScore + crystalValue 
 
 
-// start/reset game
-    //clear variables 
-    //set target score 
-    //assign a value for each crystal (.empty)
-    
-
-}
-
-)
-
-
-    $(document).ready(function(){
-
-
-        //variables
-    
-        var counter = 0;
-        var wins= 0;
-        var losses = 0;
-    
-        
-    
-        function randomNum(min,max){
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max-min +1)) + min;
-    
-        }
-    
-        var targetNumber = randomNum(40,90);
-        $("#target").html(targetNumber);
-    
-        var crystalOne= randomNum(1, 12);
-        var crystalTwo= randomNum(1, 12);
-        var crystalThree= randomNum(1, 12);
-        var crystalFour= randomNum(1, 12);
-    
-    
-        function reset(){
-            counter = 0;
-            $("#target").html(targetNumber);
-            targetNumber = randomNum(40,90);
-            $("#playerScore").text(counter);
-            crystalOne= randomNum(1, 12);
-            crystalTwo= randomNum(1, 12);
-            crystalThree= randomNum(1, 12);
-            crystalFour= randomNum(1, 12);
-    
-    
-        }
-    
-        
-        $("#greenGem").on("click", function(){
-            counter += crystalOne;
-            WinLoose();
-            $(playerScore).text(counter);
-            
-        });
-    
-        $("#whiteGem").on("click", function(){
-            counter += crystalTwo;
-            WinLoose();
-            $(playerScore).text(counter);
-            
-    
-        });
-    
-        $("#yellowGem").on("click", function(){
-            counter += crystalThree;
-            WinLoose();
-            $(playerScore).text(counter);
-            
-    
-        });
-    
-        $("#redGem").on("click", function(){
-            counter += crystalFour;
-            WinLoose();
-            $(playerScore).text(counter);
-            
-    
-        });
-    
-        function WinLoose (){
-    
-        if (counter === targetNumber){
-            wins += 1;
-            alert("Winner!");
-            $(win).text(wins);
-            reset();
-    
-        } else if(counter > targetNumber){
-            losses += 1;
-            alert("You suck sooo bad!");
-            $(loss).text(losses);
-            reset();
-        }
-        
-    }
-        
-    })
+//Global Variables: wins, losses,target score
+  var win = 0;
+  var lost = 0;
+  var RandomNum;
+  var result = 0;
+  var currentScore = 0;
+  var crystalValue = 0;
+  
+  var resetAndStart = function () {
+      $("#crystals").empty();
+  
+  //Generate Random Numbeer 
+      RandomNum = Math.floor(Math.random() * (120 - 19) + 19)
+  //This random number is then sent to screen 
+      $("#result").html('Target Number: ' + RandomNum);
+  //for loop to run through number options and create crystals 
+      for (var i = 0; i < 4; i++) {
+          var random = Math.floor(Math.random() * 11) + 1;
+  
+          var imageCrystal = $("<img>");
+          imageCrystal.attr("src", "https://static.playbill.com/dims4/default/8261b25/2147483647/resize/x250%3C/quality/90/?url=http%3A%2F%2Fplaybill-brightspot.s3.amazonaws.com%2F2d%2F09%2F0134fa7a469e9d03ea9746449a66%2FBilly-Crystal-06.jpg");
+          imageCrystal.attr('data-crystalValue', random);
+          imageCrystal.addClass("crystal-image");
+          $("#crystals").append(imageCrystal);
+      }
+  
+      $("#currentScore").html("Total Score: " + currentScore);
+  
+  }
+  
+   resetAndStart();
+  //click event for crystal -not operating 
+  $(document).on('click', ".crystal-image", function () {
+      console.log ("works")
+  
+  
+      // var crystalValue = ($(this).attr("data-crystalValue"));
+      // // crystalValue += parseInt(crystalValue);
+  
+      // var num = parseInt($(this).attr('data-crystalValue'));
+  
+      // currentScore += num
+      // console.log(num)
+  
+  
+      var crystalValue = ($(this).attr("data-crystalvalue"));
+      crystalValue += parseInt(crystalValue);
+  
+      $("#currentScore").html("Total Score: " + currentScore);
+  
+  
+      if (currentScore > result) {
+          lost++;
+  
+          currentScore = 0;
+  
+          // resetAndStart();
+  
+  
+      }
+      else if (currentScore === result) {
+          win++;
+          currentScore = 0;
+  
+          // resetAndStart ();
+      }
+  
+  
+  });
+  
+  
+  
